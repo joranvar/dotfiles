512c05d6578a90a161430dde61a679765af422f1
joranvar <bart.post@gmail.com>
2015-06-07 14:46:50 +0200

    Add xmobar to xmonad with a basic config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,20 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
+import System.IO
 
-main = xmonad $ defaultConfig
-     { modMask = mod4Mask
-     }
+main = do
+    xmobar <- spawnPipe "/usr/bin/env xmobar"
+    xmonad $ defaultConfig
+        { modMask = mod4Mask
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmobar
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+        ]
