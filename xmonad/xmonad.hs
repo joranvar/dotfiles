6c2be55db581f380dbb0d257101c90806e229f23
joranvar <bart.post@gmail.com>
2015-06-07 17:35:06 +0200

    Fix display ordering

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import XMonad.Actions.PhysicalScreens
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -16,5 +17,7 @@ main = do
             , ppTitle = xmobarColor "green" "" . shorten 50
             }
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
-        ]
+        ( [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+          ] ++
+          [ ((mod4Mask .|. mask, key), f sc) | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+                                             , (f, mask) <- [(viewScreen, 0), (sendToScreen, shiftMask)] ] )
