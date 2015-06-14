fc2c140c481c81796eec888b05f55c7701b2f4e4
joranvar <bart.post@gmail.com>
2015-06-14 07:24:52 +0200

    Link files in xmonad dir instead of complete dir

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 mypath=${0:a:h}
 
-for i in antigen xmobarrc xmonad zshrc; do
+for i in antigen xmobarrc xmonad/xmonad.hs zshrc; do
     mkdir -p ~/.${i:h}
     [[ -a ~/.$i ]] || ln -s $mypath/$i ~/.$i
 done
