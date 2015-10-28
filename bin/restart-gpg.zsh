07966157df6f3030b51634c4b17dd2831c7e45e0
joranvar <bart.post@gmail.com>
2015-10-28 13:36:57 +0100

    Export the changed variables for caller

--- bin/restart-gpg.zsh
+++ bin/restart-gpg.zsh
@@ -2,3 +2,6 @@
 pkill gpg-agent
 source ~/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git/plugins/gpg-agent/gpg-agent.plugin.zsh
 emacsclient -e \(gpg-agent-reload\ \"$GPG_AGENT_INFO\"\ \"$SSH_AUTH_SOCK\"\)
+export GPG_AGENT_INFO
+export SSH_AUTH_SOCK
+export SSH_AGENT_PID
