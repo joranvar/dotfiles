79337aac4f21fe4ac4328d9407ef5d6be2e12ccd
joranvar <bart.post@gmail.com>
2015-12-26 22:09:44 +0100

    Setup haskell editing in emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,6 +13,7 @@
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type 'cabal-repl)
  '(helm-external-programs-associations
    (quote
     (("mkv" . "vlc --play-and-exit")
@@ -195,21 +196,47 @@ Based on bh/skip-non-stuck-projects from Bernd Hansen."
 
 (use-package nix-mode)
 
-(use-package hi2
-  :ensure t)
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'haskell-mode-hook 'rainbow-delimiters-mode))
+
+;; (use-package hindent
+;;   :ensure t
+;;   :config
+;;   (add-hook 'haskell-mode-hook #'hindent-mode))
+
+(use-package ghc
+  :ensure t
+  :config
+  (add-hook 'haskell-mode-hook (lambda () (ghc-init))))
 
 (use-package haskell-mode
   :ensure t
+  :defer t
   :config
   (require 'haskell-interactive-mode)
   (require 'haskell-process)
+  (require 'haskell-indentation)
   (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-  (add-hook 'haskell-mode-hook #'hi2-mode))
+  (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+  (add-to-list 'company-backends 'company-ghc)
+  (setq company-ghc-show-info t)
+  (progn
+    (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+    (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+    (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+    (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+    (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+    (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+    (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space))
+  (eval-after-load 'haskell-cabal '(progn
+                                     (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+                                     (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+                                     (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+                                     (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal))))
 
 (use-package powershell
-  :ensure t)
-
-(use-package ghc
   :ensure t)
 
 (use-package fsharp-mode
