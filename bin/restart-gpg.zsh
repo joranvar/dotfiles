6f0c42ee7b303a94ba59ce80621b1214a25b23ca
joranvar <bart.post@gmail.com>
2016-05-21 07:11:05 +0200

    Restart gpg-agent manually
    See:
    https://github.com/NixOS/nixpkgs/commit/c4018c0e65f825584f6154fd91b85d7ad6fe9896

--- bin/restart-gpg.zsh
+++ bin/restart-gpg.zsh
@@ -1,7 +1,10 @@
 #! /usr/bin/env zsh
 pkill gpg-agent
-source ~/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh.git/plugins/gpg-agent/gpg-agent.plugin.zsh
+gpg-connect-agent /bye
+GPG_TTY=$(tty)
+export GPG_TTY
+unset SSH_AGENT_PID
+export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh"
 emacsclient -e \(gpg-agent-reload\ \"$GPG_AGENT_INFO\"\ \"$SSH_AUTH_SOCK\"\)
 export GPG_AGENT_INFO
 export SSH_AUTH_SOCK
-export SSH_AGENT_PID
