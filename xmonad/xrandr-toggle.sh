3055857269a1ac0d7f53dfbe489723d7a1ae6059
joranvar <bart.post@gmail.com>
2016-09-14 11:35:17 +0200

    Show what screens are on and off, and which modes

--- xmonad/xrandr-toggle.sh
+++ xmonad/xrandr-toggle.sh
@@ -2,12 +2,14 @@
 
 screens=$(xrandr -q | grep ".* disconnected" | cut -f1 -d' ' | grep -v "eDP1")
 for s in $(echo $screens) ; do
+    echo $s off
     xrandr --output $s --off
 done
 
 screens=$(xrandr -q | grep ".* connected" | cut -f1 -d' ' | grep -v "eDP1")
 for s in $(echo $screens) ; do
     mode=$(xrandr -q | sed '1,/'$s' connected/d;/.* connected/,$d' | head -n 1 | cut -d' ' -f4)
+    echo $s $mode
     xrandr --output $s --left-of eDP1 --mode $mode
 done
 
