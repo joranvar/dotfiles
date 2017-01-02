2a8e3debf29536f9349e429841ca97bbe1438b18
joranvar <bart.post@gmail.com>
2017-01-02 19:41:58 +0100

    Display all resolutions at once

--- xmonad/xrandr-toggle.sh
+++ xmonad/xrandr-toggle.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/env zsh
 
+typeset -A notifications
+
 screens=$(xrandr -q | grep ".* disconnected" | cut -f1 -d' ' | grep -v "eDP1")
 for s in $(echo $screens) ; do
     echo $s off
-    notify-send -t 1000 $s off
+    notifications+="$s off\n"
     xrandr --output $s --off
 done
 
@@ -11,10 +13,11 @@ screens=$(xrandr -q | grep ".* connected" | cut -f1 -d' ' | grep -v "eDP1")
 for s in $(echo $screens) ; do
     mode=$(xrandr -q | sed '1,/'$s' connected/d;/.* connected/,$d' | head -n 1 | cut -d' ' -f4)
     echo $s $mode
-    notify-send -t 1000 $s $mode
+    notifications+="$s $mode\n"
     xrandr --output $s --left-of eDP1 --mode $mode
 done
 
+notify-send -t 1000 "New Resolutions" ${notifications[*]}
 pkill -USR1 xmobar
 pkill trayer
 (nohup trayer --SetPartialStrut true --edge top --align right --width 10 --height 14 --transparent true --alpha 0 --tint black > /dev/null 2> /dev/null)&
