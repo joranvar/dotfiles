c640e8f94c41e30e910a166572201ac692e4779b
joranvar <bart.post@gmail.com>
2017-03-11 12:19:05 +0100

    Setup package-archives before loading org

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,6 +5,15 @@
 ;; As Emacs' org-babel is the vehicle that carries the rest of the dotfiles as well, this is taken care of, here.
 
 ;;; Code:
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa"))
+
+(setq package-archives '(("gnu"          . "http://elpa.gnu.org/packages/")
+                         ("melpa-stable" . "http://stable.melpa.org/packages/")
+                         ("melpa"        . "http://melpa.org/packages/")
+                         ("org"          . "http://orgmode.org/elpa/")))
+
+(package-initialize)
+
 (require 'org)
 
 (let ((joranvar:init-path (file-name-directory (or load-file-name buffer-file-name))))
