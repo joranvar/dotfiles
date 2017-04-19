e1908ebaba30b30b8b977bb1315ac9eceab432cb
joranvar <bart.post@gmail.com>
2017-04-19 07:13:55 +0200

    Add literal org files to install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 mypath=${0:a:h}
 
-for i in antigen config/git config/mpd config/taffybar conkerorrc emacs.d/init.el emacs.d/lisp gnupg/scdaemon.conf mozrepl-conkeror.js stack/config.yaml xmobarrc xmonad/xmonad.hs xmonad/default.nix xmonad/shell.nix xprofile zshrc; do
+for i in antigen config/git config/mpd config/taffybar conkerorrc emacs.d/init.el emacs.d/init.org.el emacs.d/init.org emacs.d/lisp gnupg/scdaemon.conf mozrepl-conkeror.js stack/config.yaml xmobarrc xmonad/xmonad.hs xmonad/default.nix xmonad/shell.nix xprofile zshrc; do
     echo $mypath/$i "->" ~/.$i
     mkdir -p ~/.${i:h}
     [[ -a ~/.$i ]] || ln -s $mypath/$i ~/.$i
