021990685468900fffa016115f3f9d56b3d68644
joranvar <bart.post@gmail.com>
2017-09-22 16:37:11 +0200

    Toggle compton focused_force with mod4 + shift + f

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -26,7 +26,7 @@ import XMonad (spawn, sendMessage)
 import XMonad.Util.SpawnOnce (spawnOnce)
 
 -- Keys
-import XMonad (mod4Mask, (.|.), shiftMask, xK_l, xK_b, xK_p, xK_q, xK_x, xK_w, xK_r, xK_e, xK_s, KeySym, ButtonMask)
+import XMonad (mod4Mask, (.|.), shiftMask, xK_l, xK_b, xK_p, xK_q, xK_x, xK_w, xK_r, xK_e, xK_s, xK_f, KeySym, ButtonMask)
 import XMonad.Util.EZConfig (additionalKeys)
 
 -- WMName (for Java swing GUI)
@@ -100,6 +100,7 @@ myKeys = flip additionalKeys $
    , ((0,                      xF86XK_AudioMute        ), void toggleMute)
    , ((mod4Mask .|. shiftMask, xK_b                    ), spawn "pkill redshift || redshift -l 50.9342277:-5.7725223")
    , ((mod4Mask,               xK_q                    ), spawn "cd ~/.xmonad ; nix-shell --pure --command 'ghc --make xmonad.hs -i -ilib -fforce-recomp -v0 -o xmonad-x86_64-linux' && xmonad --restart")
+   , ((mod4Mask .|. shiftMask, xK_f                    ), spawn "~/bin/compton.set.focused_force $((($(~/bin/compton.get.focused_force | awk '{print $2}') + 1) % 3))")
    ] ++
    [ ((mod4Mask .|. mask, key), f sc) | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
                                       , (f, mask) <- [(viewScreen, 0), (sendToScreen, shiftMask)] ]
