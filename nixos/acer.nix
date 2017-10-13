6d44bbe5cd9ae961fe239e8fa1cf0696ee06a4c1
joranvar <bart.post@gmail.com>
2017-10-13 14:32:08 +0200

    Add Acer config

---
+++ nixos/acer.nix
@@ -0,0 +1,53 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [ ];
+
+  # Use the gummiboot efi boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.extraModprobeConfig = ''
+    # options snd_hda_intel enable=0,1
+    # options i915 modeset=1 i915_enable_rc6=7 i915_enable_fbc=1 lvds_downclock=1
+    # options snd slots=snd-hda-intel
+    options snd-hda-intel index=0 id=PCH
+    options snd-hda-intel index=1 id=HDMI
+  '';
+  boot.blacklistedKernelModules = [ "snd_pcsp" "nouveau" ];
+  boot.kernelModules = [ "intel_agp" "i915" ];
+  hardware.pulseaudio.enable = true;
+
+  hardware.opengl.driSupport32Bit = true;
+
+  hardware.enableAllFirmware = true;
+
+  networking.hostName = "lapbart"; # Define your hostname.
+  networking.hostId = "3d14756b";
+  # networking.wireless.enable = true;  # Enables wireless.
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
+  networking.firewall.allowedTCPPorts = [ 445 139 ];
+  networking.firewall.allowedUDPPorts = [ 137 138 ];
+
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
+    xrandrHeads = [ "HDMI1" "eDP1" ];
+    resolutions = [ { x = 2560; y = 1440; } { x = 1920; y = 1080; } ];
+  };
+
+  # Power buttons.
+  services.logind.extraConfig = ''
+    HandlePowerKey=hibernate
+    PowerKeyIgnoreInhibited=yes
+  '';
+
+  virtualisation.libvirtd.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+}
