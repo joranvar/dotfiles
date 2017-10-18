db5d9502e1db62331978c4a5b1e8046548a28d2f
joranvar <bart.post@gmail.com>
2017-10-18 08:56:23 +0200

    Use new xrandr-toggle (doesn't really use xrandr)

--- xmonad/xrandr-toggle.sh
+++ xmonad/xrandr-toggle.sh
@@ -4,45 +4,18 @@ typeset -A notifications
 
 pkill taffybar
 
-export TAFFY_SCREEN=0
-if [[ $(xrandr -q | grep ".* connected" | wc -l) -eq 1 ]]; then
-    export TAFFY_MAIN_SCREEN=0
+work=$(xrandr --verbose | grep 00ffffffffffff000472dd035f627040)
+if [[ $work ]]; then
+    nvidia-settings -l --config=~/tmp/xconfig-work
+    export TAFFY_MAIN_SCREEN=2
 else
+    nvidia-settings -l --config=~/tmp/xconfig-home
     export TAFFY_MAIN_SCREEN=1
 fi
 
-order=($*)
-if [[ -z $order ]]; then
-    order=("DP-4" "HDMI-0" "DP-1")
-fi
-
-active=$(xrandr -q | grep ".* connected" | cut -f1 -d' ')
-inactive=$(xrandr -q | grep ".* disconnected" | cut -f1 -d' ')
-
-for s in $order ; do
-    echo $s ${active[(I)$s]}
-    if [[ ${active[(I)$s]} -gt 0 ]]; then
-        mode=$(xrandr -q | sed '1,/'$s' connected/d;/.* connected/,$d' | head -n 1 | cut -d' ' -f4)
-        echo $s $mode
-        notifications+="$s $mode\n"
-        echo $s ${order[(I)$s]}
-        if [[ ${order[(I)$s]} -eq 1 ]]; then
-            xrandr --output $s --mode $mode
-        else
-            xrandr --output $s --right-of $prev --mode $mode
-        fi
-        taffybar&
-        prev=$s
-        TAFFY_SCREEN=$(($TAFFY_SCREEN + 1))
-    fi
+active=$(xrandr -q | grep ".* connected" | wc -l)
+for s in $(seq 0 $(($active - 1))); do
+    echo $s
+    export TAFFY_SCREEN=$s
+    taffybar&
 done
-
-for s in $(echo $inactive) ; do
-    TAFFY_SCREEN=$(($TAFFY_SCREEN + 1))
-    mode=$(xrandr -q | sed '1,/'$s' connected/d;/.* connected/,$d' | head -n 1 | cut -d' ' -f4)
-    echo $s $mode
-    notifications+="$s off\n"
-    xrandr --output $s --off
-done
-
-notify-send -t 1000 "New Resolutions" ${notifications[*]}
