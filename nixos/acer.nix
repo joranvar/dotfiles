92562837291b83f1d92b36009d3d3a7a074d2297
joranvar <bart.post@gmail.com>
2017-12-29 10:02:58 +0100

    Add displaylink driver

--- nixos/acer.nix
+++ nixos/acer.nix
@@ -38,7 +38,7 @@
 
   # Enable the X11 windowing system.
   services.xserver = {
-    videoDrivers = [ "nvidia" ];
+    videoDrivers = [ "nvidia" "displaylink" ];
     xrandrHeads = [ "HDMI1" "eDP1" ];
     resolutions = [ { x = 2560; y = 1440; } { x = 1920; y = 1080; } ];
   };
